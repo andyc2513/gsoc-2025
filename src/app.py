6a3198503f4f27eff698a15ed6340b21ce9ece74
AndyC <ychen276@students.cs.ubc.ca>
2025-06-10 21:40:23 -0700

    added model loading

--- src/app.py
+++ src/app.py
@@ -1,11 +1,27 @@
 import torch
 from huggingface_hub import login
 from collections.abc import Iterator
-from transformers import AutoProcessor, Gemma3ForConditionalGeneration, TextIteratorStreamer
+from transformers import Gemma3ForConditionalGeneration, TextIteratorStreamer, Gemma3Processor
 import spaces
 from threading import Thread
 import gradio as gr
 import os
+from dotenv import load_dotenv, find_dotenv
 import cv2
 from loguru import logger
 from PIL import Image
+
+dotenv_path = find_dotenv()
+
+load_dotenv(dotenv_path)
+
+model_id = os.getenv("MODEL_ID", "google/gemma-3-4b-it")
+
+input_processor = Gemma3Processor.from_pretrained(model_id)
+
+model = Gemma3ForConditionalGeneration.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+    attn_implementation="eager",
+)
