edc751600ab1407a70819ca71e2d85a046837568
AndyC <ychen276@students.cs.ubc.ca>
2025-06-10 17:51:59 -0700

    adjusted import

--- src/app.py
+++ src/app.py
@@ -1,16 +1,11 @@
 import torch
 from huggingface_hub import login
 from collections.abc import Iterator
-from transformers import Gemma3ForCausalLM, AutoTokenizer, TextIteratorStreamer
-import time
+from transformers import AutoProcessor, Gemma3ForConditionalGeneration, TextIteratorStreamer
 import spaces
 from threading import Thread
 import gradio as gr
 import os
 import cv2
-import gradio as gr
-import spaces
-import torch
 from loguru import logger
 from PIL import Image
-from transformers import AutoProcessor, Gemma3ForConditionalGeneration, TextIteratorStreamer
